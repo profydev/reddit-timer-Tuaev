97a42f0db54ad6910717a9f0b8e8ba3922ef2a12
Soslan <sparksoslan@gmail.com>
2021-04-04 15:56:51 +0100

    Add Router and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/search">
+          Search
+        </Route>
+        <Route exact path="/">
+          React App
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;

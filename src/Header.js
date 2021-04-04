e09b018125fe464064d00a1ce53c91c3fea0fd92
Soslan <sparksoslan@gmail.com>
2021-04-04 16:49:24 +0100

    Add navbar image and links

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import logo from './icons/logo.svg';
 
 function Header() {
-  return <header>Header Placeholder</header>;
+  return (
+    <div className="navbar">
+      <div className="container flex">
+        <RouterLink to="/">
+          <img src={logo} alt="logo" />
+        </RouterLink>
+        <nav>
+          <ul>
+            <li>
+              <RouterLink to="#how-it-works">How it works</RouterLink>
+              <RouterLink to="#about">About</RouterLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 export default Header;
